4675cc0160645ebcb5a2e56747dad603df2f9110
NotCybot <141260979+NotCybot@users.noreply.github.com>
2024-11-18 16:56:47 +0100

    added 07-composite-observer-jfx

---
+++ 07-composite-observer-jfx/src/main/java/de/thro/inf/prg3/a07/controllers/MainController.java
@@ -0,0 +1,108 @@
+package de.thro.inf.prg3.a07.controllers;
+
+import de.thro.inf.prg3.a07.api.OpenMensaAPI;
+import de.thro.inf.prg3.a07.model.Meal;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+public class MainController implements Initializable {
+
+	private OpenMensaAPI api;
+	private ObservableList<Meal> meals = FXCollections.observableArrayList();
+
+
+	// use annotation to tie to component in XML
+	@FXML
+	private Button btnRefresh;
+
+	@FXML
+	private ListView<String> mealsList;
+
+	@FXML
+	private CheckBox chkVegetarian;
+
+	@FXML
+	private DatePicker datePicker;
+
+	public MainController() {
+		// initialize Retrofit
+		Retrofit retrofit = new Retrofit.Builder()
+			.addConverterFactory(GsonConverterFactory.create())
+			.baseUrl("https://openmensa.org/api/v2/")
+			.build();
+
+		api = retrofit.create(OpenMensaAPI.class);
+	}
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// set the event handler (callback)
+		btnRefresh.setOnAction(this::doUpdate);
+
+		chkVegetarian.selectedProperty().addListener((observable, oldValue, newValue) -> doUpdate(null));
+
+	}
+
+	private void doUpdate(ActionEvent actionEvent) {
+		//  implement update logic
+		System.out.println("Updating...");
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+		String today = sdf.format(new Date());
+
+		System.out.println("Today: " + today);
+		api.getMeals(229, today).enqueue(new Callback<List<Meal>>() {
+			@Override
+			public void onResponse(Call<List<Meal>> call, Response<List<Meal>> response) {
+				if (!response.isSuccessful() || response.body() == null) {
+					System.err.println("Request failed: " + response.message());
+					System.err.println("HTTP Status Code: " + response.code());
+					return;
+				}
+
+				// run async update!
+				Platform.runLater(() -> {
+					Platform.runLater(() -> {
+						meals.clear();
+						meals.addAll(response.body());
+
+						// Stringify the meals
+						List<String> mealString = meals.stream()
+							.filter(meal -> !chkVegetarian.isSelected() || meal.isVegetarian())
+							.map(Meal::toString)
+							.collect(Collectors.toList());
+
+						mealsList.setItems(FXCollections.observableArrayList(mealString));
+					});
+				});
+			}
+
+			@Override
+			public void onFailure(Call<List<Meal>> call, Throwable t) {
+				System.out.println("No Meals today :(");
+			}
+		});
+	}
+}
---
+++ 07-composite-observer-jfx/src/test/java/de/thro/inf/prg3/a07/tests/OpenMensaAPITests.java
@@ -0,0 +1,65 @@
+package de.thro.inf.prg3.a07.tests;
+
+import de.thro.inf.prg3.a07.api.OpenMensaAPI;
+import de.thro.inf.prg3.a07.model.Meal;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class OpenMensaAPITests {
+
+	private static final Logger logger = LogManager.getLogger(OpenMensaAPITests.class);
+	private static OpenMensaAPI openMensaAPI;
+
+	@BeforeAll
+	static void setup() {
+
+		// use this to intercept all requests and output them to the logging facilities
+		HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+		loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+		OkHttpClient client = new OkHttpClient.Builder()
+			.addInterceptor(loggingInterceptor)
+			.build();
+
+		Retrofit retrofit = new Retrofit.Builder()
+			.addConverterFactory(GsonConverterFactory.create())
+			.baseUrl("https://openmensa.org/api/v2/")
+			.client(client)
+			.build();
+
+		openMensaAPI = retrofit.create(OpenMensaAPI.class);
+	}
+
+	@Test
+	void testGetMeals() throws IOException {
+		//  prepare call
+		Call<List<Meal>> call = openMensaAPI.getMeals(229, "2017-11-22");
+
+		//  execute the call synchronously
+		retrofit2.Response<List<Meal>> response = call.execute();
+
+		//  unwrap the body
+		List<Meal> meals = response.body();
+
+		assertNotNull(meals);
+		assertNotEquals(0, meals.size());
+
+		for(Meal m : meals){
+			logger.info(m.toString());
+		}
+	}
+
+}
